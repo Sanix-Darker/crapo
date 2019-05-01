0c45b376fb6a76e2f9d73ace5c63505c722a1900
Sanix-darker <saadjioa@gmail.com>
2019-05-01 14:34:09 +0100

    Adding tenoga on crapo

---
+++ default/install_on_windows.bat
@@ -0,0 +1,3 @@
+pip install --upgrade setuptools pip
+pip install setuptools --upgrade --ignore-installed
+pip install -r requirements.txt
--- src/install_on_linux.sh
+++
@@ -1,4 +0,0 @@
-pip install --upgrade setuptools pip
-pip install setuptools --upgrade --ignore-installed
-pip install -r requirements.txt
-sh starter_crapo_for_linux.sh
